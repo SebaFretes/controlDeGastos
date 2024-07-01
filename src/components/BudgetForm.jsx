ca56eb9c70f11b39c3a655b804f3f0c34a4131ee
Sebastián Fretes <sebafretesca@gmail.com>
2024-07-01 15:13:50 -0400

    form created

--- src/components/BudgetForm.jsx
+++ src/components/BudgetForm.jsx
@@ -1,5 +1,28 @@
+import { useState } from "react"
+
 export const BudgetForm = () => {
+
+    const [budget, setBudget] = useState(0);
+
+    const handleChange = (e) => {
+        setBudget(e.target.value);
+    }
+
   return (
-    <div>BudgetForm</div>
+    <form className="space-y-5">
+        <div className="flex flex-col space-y-5">
+            <label className="text-center text-blue-600 font-bold">Definir Presupuesto</label>
+            <input type="number"
+            className="w-full bg-white border border-gray-200 p-2"
+            placeholder="Definí tu presupuesto"
+            value={budget}
+            onChange={handleChange}
+            />
+        </div>
+        <input type="submit"
+        value='Definir Presupuesto'
+        className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase"
+        />
+    </form>
   )
 }
