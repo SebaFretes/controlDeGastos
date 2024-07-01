ebbb70bb964cc292eb23eade1ee6a4460a7f6a1b
Sebastián Fretes <sebafretesca@gmail.com>
2024-07-01 14:28:39 -0400

    BudgetForm created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-
+import { BudgetForm } from "./components/BudgetForm"
 
 export const App = () => {
   return (
-    <h1>Control de Gastos</h1>
+    <>
+    <div className="bg-blue-600 py-8 max-h-72">
+      <h1 className="text-white text-center text-3xl font-black uppercase">Planificador de Gastos</h1>
+
+    </div>
+    
+    <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
+      <BudgetForm />
+    </div>
+    
+    
+    </>
+    
+    
   )
 }
