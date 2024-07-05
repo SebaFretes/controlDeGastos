34dcb54909a8140cae251d44626fe40ab89ecf37
Sebastián Fretes <sebafretesca@gmail.com>
2024-07-05 14:42:04 -0400

    AmountDisplay component created

---
+++ src/components/AmountDisplay.jsx
@@ -0,0 +1,13 @@
+
+
+export const AmountDisplay = ({label, amount}) => {
+    return (
+        <>
+            <p className=" text-2xl text-blue-600 font-bold">
+                {label}: {''}
+                <span className="font-black text-black">${amount}</span>
+            </p>
+            
+        </>        
+    )
+}
--- src/components/BudgetTracker.jsx
+++ src/components/BudgetTracker.jsx
@@ -1,7 +1,32 @@
-
+import { AmountDisplay } from "./AmountDisplay";
 
 export const BudgetTracker = () => {
     return(
-        <h1>Budget Tracker</h1>
+        <>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+            <div className="flex justify-center">
+                <img src="/grafico.jpg" alt="gastos img"/>
+            </div>
+
+            <div className="flex flex-col justify-center items-center gap-8">
+                <button type="button" className="bg-red-600 w-full p-2 text-white uppercase font-bold rounded-lg">
+                    Resetear App
+                </button>
+            </div>
+
+            <div>
+                <AmountDisplay label="Presupuesto" amount={300}/>
+            </div>
+
+            <div>
+                <AmountDisplay label="Gastado" amount={100}/>
+            </div>
+
+            <div>
+                <AmountDisplay label="Disponible" amount={200}/>
+            </div>
+
+        </div>
+        </>
     )
 }
