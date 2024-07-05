12a5ee201a02419429af7e3c531888e8cc50eb05
Sebastián Fretes <sebafretesca@gmail.com>
2024-07-05 14:00:41 -0400

    img added in public folder

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,8 @@ export const App = () => {
     
     <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
       {state.budget <= 0 ? <BudgetForm /> : <BudgetTracker />}
-    </div>    
+    </div>
+    
     </>
   )
 }
