cda1b5ab9d6bb0856a9c7232ace89af295f19287
Sebastián Fretes <sebafretesca@gmail.com>
2024-07-06 15:22:12 -0400

    ExpenseForm created and designed, functionality still needs to be worked on

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,3 +1,4 @@
+import { categories } from "../data/categories"
 
 export const ExpenseForm = () => {
     return (
@@ -18,6 +19,24 @@ export const ExpenseForm = () => {
                     </label>
                     <input type="number" className="bg-slate-100 p-2" placeholder="Agregá la cantidad"/>
                 </div>
+
+                <div className="flex flex-col gap-2">
+                    <label className="text-xl">
+                        Categoría:
+                    </label>
+                    <select id="category" className="bg-slate-100 p-2" name="category">
+                        <option value="">--Seleccioná la categoría--</option>
+                        {categories.map(cat => (
+                            <option key={cat.id} value={cat.id}>
+                                {cat.name}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+
+                <input type="submit"
+                className="bg-blue-600 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg"
+                value={'Registrar Gasto'}/>
 
             </form>
         </>
