f4321b4d93081176061249f77f054f078dfb1cc3
Sebastián Fretes <sebafretesca@gmail.com>
2024-07-09 15:44:49 -0400

    getCategoryName & getCategoryIcon created in ExpenseList

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -1,3 +1,4 @@
+import { categories } from "../data/categories";
 import { useBudget } from "../hooks/useBudget";
 import { AmountDisplay } from "./AmountDisplay";
 
@@ -20,6 +21,17 @@ export const ExpenseList = () => {
         return new Date(date).toLocaleString('es-ES', options);
     };
 
+  
+    const getCategoryName = (categoryId) => {
+        const category = categories.find(cat => cat.id === categoryId);
+        return category.name;
+    };
+
+    const getCategoryIcon = (categoryId) => {
+        const category = categories.find(cat => cat.id === categoryId);
+        return category.icon;
+    };
+
   return (
     <div className="mt-10">
         {isEmpty ? <h1 className="text-gray-600 text-2xl font-bold">No hay gastos</h1> :
@@ -30,8 +42,10 @@ export const ExpenseList = () => {
                   <div className="bg-white shadow-lg p-10 w-full border-b border-gray-100 flex gap-5 items-center">
                     {state.expenses.map(exp => (
                         <div key={exp.id}>
-                            <h4>{exp.expenseName}</h4>
-                            <p>Compra realizada el: {formatDate(exp.date)}</p>
+                            <p className="font-bold uppercase text-slate-500">{getCategoryName(exp.category)}</p>
+                            <p>{exp.expenseName}</p>
+                            <p className="text-slate-600">Compra realizada el: {formatDate(exp.date)}</p>
+                            <img src={`/icono_${getCategoryIcon(exp.category)}.svg`} />
                             <AmountDisplay amount={exp.amount}/>
                         </div>
                     ))}
