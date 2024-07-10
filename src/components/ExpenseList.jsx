b57d533b57cb9a916fad6c0501c9c800cfddd1bb
Sebastián Fretes <sebafretesca@gmail.com>
2024-07-10 14:10:39 -0400

    the registered expenses are displayed and the design looks nice lol

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -4,7 +4,7 @@ import { AmountDisplay } from "./AmountDisplay";
 
 export const ExpenseList = () => {
 
-    const {state} = useBudget();
+    const { state } = useBudget();
 
     const isEmpty = state.expenses.length === 0;
 
@@ -21,7 +21,7 @@ export const ExpenseList = () => {
         return new Date(date).toLocaleString('es-ES', options);
     };
 
-  
+
     const getCategoryName = (categoryId) => {
         const category = categories.find(cat => cat.id === categoryId);
         return category.name;
@@ -32,26 +32,30 @@ export const ExpenseList = () => {
         return category.icon;
     };
 
-  return (
-    <div className="mt-10">
-        {isEmpty ? <h1 className="text-gray-600 text-2xl font-bold">No hay gastos</h1> :
-              <>
-                  <h1 className="text-gray-600 text-2xl font-bold my-5">
-                    Listado de Gastos
-                  </h1>
-                  <div className="bg-white shadow-lg p-10 w-full border-b border-gray-100 flex gap-5 items-center">
+    return (
+        <div className="mt-10">
+            {isEmpty ? <h1 className="text-gray-600 text-2xl font-bold">No hay gastos</h1> :
+                <>
+                    <h1 className="text-gray-600 text-2xl font-bold my-5">
+                        Listado de Gastos
+                    </h1>
                     {state.expenses.map(exp => (
-                        <div key={exp.id}>
-                            <p className="font-bold uppercase text-slate-500">{getCategoryName(exp.category)}</p>
-                            <p>{exp.expenseName}</p>
-                            <p className="text-slate-600">Compra realizada el: {formatDate(exp.date)}</p>
-                            <img src={`/icono_${getCategoryIcon(exp.category)}.svg`} />
-                            <AmountDisplay amount={exp.amount}/>
+                        <div className="bg-white shadow-lg p-10 w-full border-b border-gray-100 mb-4">
+                            <div key={exp.id} className="flex justify-between items-center gap-5 mb-5">
+                                <div className="flex items-center gap-5">
+                                    <img src={`/icono_${getCategoryIcon(exp.category)}.svg`} alt="img" className="w-20" />
+                                    <div>
+                                        <p className="font-bold uppercase text-slate-500">{getCategoryName(exp.category)}</p>
+                                        <p>{exp.expenseName}</p>
+                                        <p className="text-slate-600">Compra realizada el: {formatDate(exp.date)}</p>
+                                    </div>
+                                </div>
+                                <AmountDisplay amount={exp.amount} />
+                            </div>
                         </div>
                     ))}
-                  </div>
-              </>
-        }
-    </div>
-  )
+                </>
+            }
+        </div>
+    )
 }
